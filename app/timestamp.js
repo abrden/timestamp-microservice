297fffc35946eb1e7a2d6e1a68f48cbc47495401
Agustina <agustina.barbetta@gmail.com>
2016-02-20 05:57:26 +0000

    API model

---
+++ app/timestamp.js
@@ -0,0 +1,34 @@
+'use strict';
+var moment = require('moment');
+
+module.exports = function(app) {
+    
+    app.get('/:query', function(req, res) {
+        var date = req.params.query;
+        var unix = null;
+        var natural = null;
+        
+        // Check for unix time
+        if (+date >= 0) {
+            unix = +date;
+            natural = unixToNat(unix);
+        } 
+        
+        // Check for natural time
+        if (isNaN(+date) && moment(date, "MMMM D, YYYY").isValid()) {
+            unix = natToUnix(date);
+            natural = unixToNat(unix);
+        }
+        
+        res.send(JSON.stringify({ "unix": unix, "natural": natural }));
+        
+    });
+    
+    function natToUnix(date) {
+        return moment(date, "MMMM D, YYYY").format("X");
+    }
+    
+    function unixToNat(unix) {
+        return moment.unix(unix).format("MMMM D, YYYY");
+    }
+};
